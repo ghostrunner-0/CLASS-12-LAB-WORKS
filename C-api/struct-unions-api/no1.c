abd2cbd19dc59519442b3596c265d64dc64005c9
priyanshu begwani <138878654+ghostrunner0@users.noreply.github.com>
2023-07-19 16:22:34 +0545

    Create no1.c

---
+++ C-api/struct-unions-api/no1.c
@@ -0,0 +1,22 @@
+// WAP to input id, name and grade for 10 students. Then print them. Use array of structure concept.
+#include<stdio.h>
+struct student{
+    int id;
+    char name[100];
+    int grade;
+}std_data[10];
+int main(){
+    int i;
+    for(i=0;i<10;i++){
+        printf("Enter name of student\n");
+        scanf("%s",std_data[i].name);
+        printf("Enter %s's grade\n",std_data[i].name);
+        scanf("%d",&std_data[i].grade);
+        printf("Enter %s's id no\n",std_data[i].name);
+        scanf("%d",&std_data[i].id);
+    }  
+    printf("Name\tGrade\tID\n");
+    for(i=0;i<10;i++){
+        printf("%s\t%d\t%d\n",std_data[i].name,std_data[i].grade,std_data[i].id);
+    }
+}
