5f7bf7d8ee2e3d0a73f2d08a0d64a241bc935140
priyanshu begwani <itsmepriyanshu01@gmail.com>
2023-07-30 15:12:55 +0545

    ok

---
+++ C/struct-unions-api/no7.cpp
@@ -0,0 +1,20 @@
+//WAP to input student id, name and grade and print them. Use union concept
+#include<stdio.h>
+union d{
+    int id;
+    char name[100];
+    int grade; 
+}s;
+main()
+{
+printf("Enter student id\n");
+scanf("%d",&s.id);
+printf("Student id is %d\n",s.id);
+printf("Enter student's name\n");
+scanf("%s",s.name);
+printf("Name is : %s\n",s.name);
+printf("Enter %s's grade\n",s.name);
+scanf("%d",&s.grade);
+printf("Student's grade is %d",s.grade);
+return 0;
+}
