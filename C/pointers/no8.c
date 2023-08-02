70b6fbe50f0497f27b415f4d52e71671c33ca2bb
priyanshu begwani <itsmepriyanshu01@gmail.com>
2023-08-02 16:14:01 +0545

    Update no8.c

--- C/pointers/no8.c
+++ C/pointers/no8.c
@@ -1,1 +1,15 @@
 // WAP to print multiplication table of a number using pointer.
+#include<stdio.h>
+int main()
+{
+    int a;
+    int *k;
+    k=&a;
+    printf("Enter a number\n");
+    scanf("%d",&*k);
+    int i;
+    for(i=1;i<=10;i++){
+        printf("%d*%d=%d\n",*k,i,i**k);
+    }
+return 0;
+}
