81e6f7ba80f7f8ef356a0a8d129f27262a1eea40
notV01D <105712069+notV01D@users.noreply.github.com>
2023-08-07 15:07:10 +0545

    Add files via upload

---
+++ C/pointers/Qn6.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+void swap_val(int a, int b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+int main()
+{
+    int a = 10, b = 20;
+    printf("Before swap: a = %d, b = %d\n", a, b);
+    swap(&a, &b);
+    printf("After swap(call by reference): a = %d, b = %d\n", a, b);
+    swap_val(a, b);
+    printf("After swap(call by value): a = %d, b = %d\n", a, b);
+    return 0;
+}
