dd132d93086d6c26d8048fcf337b010ac2c21bda
priyanshu begwani <itsmepriyanshu01@gmail.com>
2023-08-09 14:09:27 +0545

    Create example2.cpp

---
+++ C/Data-file/example2.cpp
@@ -0,0 +1,29 @@
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+    FILE *k;
+    char name[100];
+    int id;
+    char choice;
+    k=fopen("test.xls","a");
+    printf("Enter data\n");
+    do
+    {
+        printf("Enter name\n");
+        scanf("%s",name);
+        printf("Enter id\n");
+        scanf("%d",&id);
+        fprintf(k,"%d\t%s\n",id,name);
+        printf("If you want to continue click'y'\n");
+        choice=getch();
+    } while (choice =='y'|| choice=='Y');
+    fclose(k);
+    fopen("test.xls","r");
+    int i=0;
+    do{
+        printf("ID=%d\tName=%s\n",id,name);
+    }while((fscanf(k,"%d%s",&id,name))!=EOF);
+    fclose(k);
+return 0;
+}
