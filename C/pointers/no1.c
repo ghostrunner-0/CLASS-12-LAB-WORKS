b36721d118dbd90d12b24ac1d3998b4392dac9d2
priyanshu begwani <itsmepriyanshu01@gmail.com>
2023-08-24 15:48:12 +0545

    Update no1.c

--- C/pointers/no1.c
+++ C/pointers/no1.c
@@ -2,15 +2,15 @@
 #include<stdio.h>
 int main()
 {
-    int a,b;
-    int *k,*j;
+    float a,b;
+    float *k,*j;
     k=&a;
     j=&b;
     printf("Enter two number\n");
-    scanf("%d%d",&*k,&*j);
-    printf("The sum of the numbers is: %d",*k+*j);
-    printf("The difference of the numbers is: %d",*k-*j);
-    printf("The product of the numbers is: %d",*k**j);
-    printf("The division of the numbers is: %d",(*k)/(*j));
+    scanf("%f%f",&*k,&*j);
+    printf("The sum of the numbers is: %0.f",*k+*j);
+    printf("The difference of the numbers is: %0.f",*k-*j);
+    printf("The product of the numbers is: %0.f",*k**j);
+    printf("The division of the numbers is: %0.2f",(*k)/(*j));
 return 0;
 }
