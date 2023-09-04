29f539e63d0f3d498262f5a0245104a755e9ddbd
priyanshu begwani <itsmepriyanshu01@gmail.com>
2023-09-04 15:42:42 +0545

    .

--- C/Data-file/no8.cpp
+++ C/Data-file/no8.cpp
@@ -12,14 +12,14 @@ int main()
 
     fp = fopen("file.txt", "r");
 
-    //ftell() function returns the current position of the file pointer.
+    //?ftell() function returns the current position of the file pointer.
     printf("Current position of the file pointer: %ld\n", ftell(fp));
 
-    //fseek() function moves the file pointer to a specified position.
+    //?fseek() function moves the file pointer to a specified position.
     fseek(fp, 3, SEEK_SET);
     printf("Current position of the file pointer after fseek(): %ld\n", ftell(fp));
 
-    //rewind() function moves the file pointer to the beginning of the file.
+    //?rewind() function moves the file pointer to the beginning of the file.
     rewind(fp);
     printf("Current position of the file pointer after rewind(): %ld\n", ftell(fp));
 
