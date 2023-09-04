6728405300b3301017f958b39b8e9acea8c6a582
priyanshu begwani <itsmepriyanshu01@gmail.com>
2023-09-04 16:01:34 +0545

    Update no7.cpp

--- C/Data-file/no7.cpp
+++ C/Data-file/no7.cpp
@@ -1,54 +1,38 @@
-#include <stdio.h>
-#include <conio.h>
+//?7. WAP to show the concept of rename() and remove() functions.
+#include<stdio.h>
+#include<conio.h>
 typedef int i;
 typedef char c;
 typedef FILE f;
 int main()
 {
-    f *k, *k1;
-    c name[100], choice;
-    i roll, searchid, flag = 0;
-    k = fopen("student.txt", "a");
-    printf("Enter ctrl+z to exit");
-    while ((scanf("%d%s", &roll, name)) != EOF)
-    {
-        fprintf(k, "%d\t%s\t\n", roll, name);
-    }
-    printf("\nwriting process completed\n");
+    f *k;
+    c name[100];
+    i grade;
+    c choice,choose,name2[100];
+    k=fopen("file1.txt","a");
+    do{
+        printf("\nEnter your Name: ");
+        scanf("%s",&name);
+        printf("\nEnter Grade: ");
+        scanf("%d",&grade);
+        fprintf(k,"%s %d\n",name,grade);
+        printf("Enter 'y' to continue\n");
+        choice=getch();
+    }while(choice== 'y'|| choice== 'Y');
     fclose(k);
-    k = fopen("student.txt", "r");
-    k1 = fopen("updatedata.txt", "w");
-    printf("Enter student rollno to be updated\n");
-    scanf("%d", &searchid);
-    while ((fscanf(k, "%d%s", &roll, name)) != EOF)
-    {
-        if (searchid == roll)
-        {
-            flag = 1;
-            printf("name=%s\t per=%f\n", name);
-            printf("enter rollno\n");
-            scanf("%d", &roll);
-            printf("Enter name");
-            scanf("%s", name);
-            fprintf(k1, "%d\t%s\n", roll, name);
-        }
-        else
-        {
-            fprintf(k1, "%d\t%s\n", roll, name);
-        }
+    printf("Press d to delete the file OR");
+    printf("Press r to rename the file\n");
+    choose=getch();
+    switch(choose){
+        case ('d'):remove("file1.txt");
+        break;
+        case ('r'):printf("Rename file(with extension) to?: ");
+        scanf("%s",name2);
+        rename("file1.txt",name2);
+        break;
+        default:
+        printf("Wrong choice\n");
     }
-    fclose(k);
-    fclose(k1);
-    remove("student.txt");
-    rename("updatedata.txt", "student.txt");
-    if (flag == 0)
-    {
-        printf("no data found to be edited\n");
-    }
-    else
-    {
-        printf("data updated sucessfully\n");
-    }
-    getch();
-    return 0;
+return 0;
 }
