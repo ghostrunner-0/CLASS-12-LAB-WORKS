6b8c59810ef4f1647ab7d8eec84c39c66dbcbc56
priyanshu begwani <itsmepriyanshu01@gmail.com>
2023-12-11 15:18:40 +0545

    Question no 11

--- Javascript/Question no 11/script.js
+++ Javascript/Question no 11/script.js
@@ -1,1 +1,3 @@
 // 11.Write a program to insert a string “We are learning JS” via form object getElementById() method. Use paragraph with id.
+let a=document.getElementById('out')
+a.innerHTML='We are learning JS'
